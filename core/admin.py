b44adfd05578e8d9fa574f933ab7bdfe13306fd3
Prashant Shukla <bharatexpress.d@gmail.com>
2025-09-11 10:26:44 +0530

    Registered Category, Product, and Cart models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Product, Cart
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category', 'stock', 'available']
+    list_filter = ['category', 'available']
+    search_fields = ['name', 'description']
+    list_editable = ['price', 'stock', 'available']
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'quantity', 'created_at']
+    list_filter = ['user']
