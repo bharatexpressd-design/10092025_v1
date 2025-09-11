0b0faf9dc3fd3aebcf6498895981742ffede6ae9
Prashant Shukla <bharatexpress.d@gmail.com>
2025-09-11 21:26:08 +0530

    Added registration/login.html for user authentication

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'core/templates'],  # Ensure this line exists
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
