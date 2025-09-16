4fe9918dc07a42679af2879c200b8577ef79fda3
Prashant Shukla <bharatexpress.d@gmail.com>
2025-09-16 22:44:45 +0530

    fix: correct indentation in ecommerce/urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,9 +1,9 @@
-# ecommerce/urls.py
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from core import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
